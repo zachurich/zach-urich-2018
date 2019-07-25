ab393e84f6d8f0bc86d44f6da9a5d7f76aee2b27
Zach Urich <re.jo@live.com>
2019-07-24 21:34:53 -0500

    Dyanimcally fetch routes for static exporting

--- next.config.js
+++ next.config.js
@@ -1,21 +1,31 @@
 const path = require("path");
 const glob = require("glob");
+const fetch = require("node-fetch");
 
 module.exports = {
-  // exportPathMap: function() {
-  //   return {
-  //     "/": { page: "/" },
-  //     "/writing": { page: "/writing" },
-  //     "/writing/guidelines-to-keep-in-mind-when-embracing-unknowns": {
-  //       page: "/post",
-  //       query: { slug: "guidelines-to-keep-in-mind-when-embracing-unknowns" }
-  //     },
-  //     "/writing/education-and-learning": {
-  //       page: "/post",
-  //       query: { slug: "education-and-learning" }
-  //     }
-  //   };
-  // },
+  async exportPathMap() {
+    const pages = {
+      "/": { page: "/" },
+      "/writing": { page: "/writing" }
+    };
+    const response = await fetch(
+      "https://zachurichblog.cdn.prismic.io/api/v2/documents/search?page=1&pageSize=20&ref=XNjSHBEAACQAeibQ"
+    );
+    const data = await response.json();
+    const posts = data.results.map(post => {
+      return {
+        [`/writing/${post.uid}`]: {
+          page: "/post",
+          query: { slug: post.uid }
+        }
+      };
+    });
+
+    posts.forEach(post =>
+      Object.entries(post).map(([path, values]) => (pages[path] = values))
+    );
+    return pages;
+  },
   webpack: (config, { dev }) => {
     config.module.rules.push(
       {
