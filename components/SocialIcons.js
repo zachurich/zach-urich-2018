2ae7bef98a3a59fc88b46d0b187ee834ab67a11f
Zach Urich <re.jo@live.com>
2020-06-25 15:45:32 -0500

    Updating projects

--- components/SocialIcons.js
+++ components/SocialIcons.js
@@ -1,37 +1,21 @@
-import React from "react";
 import { ShareButtons, ShareCounts, generateShareIcon } from "react-share";
 
-const SocialIcons = props => {
-  const {
-    FacebookShareButton,
-    GooglePlusShareButton,
-    LinkedinShareButton,
-    TwitterShareButton
-  } = ShareButtons;
+import React from "react";
+
+const SocialIcons = (props) => {
+  const { FacebookShareButton, LinkedinShareButton, TwitterShareButton } = ShareButtons;
   const FacebookIcon = generateShareIcon("facebook");
   const TwitterIcon = generateShareIcon("twitter");
-  const GooglePlusIcon = generateShareIcon("google");
   const LinkedinIcon = generateShareIcon("linkedin");
   return (
     <div className="social-icons">
       Share:
-      <FacebookShareButton
-        url={props.url}
-        quote={props.title}
-        className="social-icon"
-      >
+      <FacebookShareButton url={props.url} quote={props.title} className="social-icon">
         <FacebookIcon size={32} round />
       </FacebookShareButton>
-      <TwitterShareButton
-        url={props.url}
-        title={props.title}
-        className="social-icon"
-      >
+      <TwitterShareButton url={props.url} title={props.title} className="social-icon">
         <TwitterIcon size={32} round />
       </TwitterShareButton>
-      <GooglePlusShareButton url={props.url} className="social-icon">
-        <GooglePlusIcon size={32} round />
-      </GooglePlusShareButton>
       <LinkedinShareButton
         url={props.url}
         title={props.title}
